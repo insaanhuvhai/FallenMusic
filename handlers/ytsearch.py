afc537c0752f5a278c241a84c0f4ad1edefd8b5f
insaanhuvhai <104100121+insaanhuvhai@users.noreply.github.com>
2022-04-28 20:07:15 +0530

    Update ytsearch.py

--- handlers/ytsearch.py
+++ handlers/ytsearch.py
@@ -21,19 +21,19 @@ async def ytsearch(_, message: Message):
     await message.delete()
     try:
         if len(message.command) < 2:
-            await message.reply_text("» ɢɪᴠᴇ sᴏᴍᴇ ᴛᴇxᴛ ᴛᴏ sᴇᴀʀᴄʜ ʙᴀʙʏ!")
+            await message.reply_text("» Give Me Some Text To Proceed 🙄")
             return
         query = message.text.split(None, 1)[1]
-        m = await message.reply_text("🔎")
+        m = await message.reply_text("processing...⚡")
         results = YoutubeSearch(query, max_results=4).to_dict()
         i = 0
         text = ""
         while i < 4:
-            text += f"📌 ᴛɪᴛʟᴇ : {results[i]['title']}\n"
-            text += f"⏱ ᴅᴜʀᴀᴛɪᴏɴ : {results[i]['duration']}\n"
-            text += f"👀 ᴠɪᴇᴡs : {results[i]['views']}\n"
-            text += f"📣 ᴄʜᴀɴɴᴇʟ : {results[i]['channel']}\n"
-            text += f"🔗 ʟɪɴᴋ : https://youtube.com{results[i]['url_suffix']}\n\n"
+            text += f"♦️Title : {results[i]['title']}\n"
+            text += f"⏱ Duration : {results[i]['duration']}\n"
+            text += f"🎥 Views : {results[i]['views']}\n"
+            text += f"⚡ Channel : {results[i]['channel']}\n"
+            text += f"🔗 Link : https://youtube.com{results[i]['url_suffix']}\n\n"
             i += 1
         await m.edit(text, disable_web_page_preview=True)
     except Exception as e:
